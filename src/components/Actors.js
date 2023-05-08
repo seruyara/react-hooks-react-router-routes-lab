7d71b473dcf45a261b0052e409018efcc36c3be6
Sheila Seruya <Sheilaseruya@gmail.com>
2023-05-08 20:25:29 +0300

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { actors } from "../data";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Actors from "./Actors";
+import Directors from "./Directors";
+import Movies from "./Movies";
 
-function Actors() {
-  return <div>{/*{code here}*/}</div>;
+function App() {
+  return <div>
+          <NavBar/>
+          <Switch>
+          <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route path="/movies">
+                <Movies/>
+                  </Route>
+            <Route path="/directors">
+                <Directors/>
+                  </Route>
+            <Route path="/actors">
+              <Actors/>
+                  </Route>
+          </Switch>
+         </div>;
 }
 
-export default Actors;
+export default App;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,18 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie)=>(
+    <div>
+      {movie.title}
+         {movie.time} 
+      <ul>
+          <li>{movie.genres}</li>
+      </ul>
+    </div>))}
+  </div>
+)}
 
 export default Movies;
